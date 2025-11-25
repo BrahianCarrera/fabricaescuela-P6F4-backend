1c09d5d2c9c431cfa91bf28fff5a2f83810901c4
BrahianCarrera <brahian.carrera@udea.edu.co>
2025-11-24 21:54:08 -0500

    jej

--- src/main/java/com/fabricaescuela/models/dto/NovedadDto.java
+++ src/main/java/com/fabricaescuela/models/dto/NovedadDto.java
@@ -3,8 +3,7 @@ package com.fabricaescuela.models.dto;
 import java.time.LocalDateTime;
 
 public record NovedadDto(
-        String descripcion,
-        LocalDateTime fecha,
-        String registradoPor
-) {}
-
+                String descripcion,
+                LocalDateTime fecha,
+                String registradoPor) {
+}
